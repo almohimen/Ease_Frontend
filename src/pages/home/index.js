2c0275be99cdf0f51d3d96ffdfab3c726d387d55
Mohimen Islam <almohimen98@gmail.com>
2022-11-17 02:21:47 +0600

    updt

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,12 @@
 // import React, { useRef, useState } from "react";
 import { useSelector } from "react-redux";
+import CreatePost from "../../components/createPost";
 import Header from "../../components/header";
 import LeftHome from "../../components/home/left";
 import RightHome from "../../components/home/right";
+// import Stories from "../../components/home/stories";
 // import useClickOutside from "../../helpers/clickOutside";
+import './style.css'
 
 export default function Home() {
   const { user } = useSelector((user) => ({ ...user }));
@@ -12,6 +15,10 @@ export default function Home() {
       <Header />
       <LeftHome user={user} />
       <RightHome user={user} />
+      <div className="home_middle">
+        {/* <Stories/> */}
+        <CreatePost/>
+      </div>
     </div>
   );
 }
